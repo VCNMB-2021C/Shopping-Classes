33d8cc0e990d5d3dbae1978153d23f0769220b98
rwanv <rwanv@REECE-LAPTOP>
2021-03-30 13:30:49 +0200

    Added Generic Classes 1

---
+++ prjGenericClasses/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjGenericClasses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            Stack_Items<String> si = new Stack_Items<string>(5);
+            si.Push("Anthem");
+            si.Push("Rainbow six siege");
+            si.Push("Counter-Strike Global Offensive");
+            si.Push("Starbound");
+            si.Push("Fort nut");
+
+            String strDisplay = si.Display();
+            MessageBox.Show("The following games have been captured:\n\n"
+                + strDisplay);
+        }
+    }
+}
---
+++ prjGenericClasses/Stack_Items.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prjGenericClasses
+{
+    class Stack_Items<T>
+    {
+        private static object[] item;
+        private static int stackPointer = 0;
+        private string strDisplay;
+
+        public Stack_Items(int size)
+        {
+            item = new object[size];
+        }
+        public string Display()
+        {
+            for (int x = 0; x < stackPointer; x++)
+            {
+                strDisplay += item[x] + "\n";
+            }
+            return strDisplay;
+        }
+        public void Push(object anItem)
+        {
+            item[stackPointer] = anItem;
+            stackPointer++;
+        }
+    }
+}
